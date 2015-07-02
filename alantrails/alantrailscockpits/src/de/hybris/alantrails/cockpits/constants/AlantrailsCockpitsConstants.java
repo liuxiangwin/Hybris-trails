d3874890ecda71967eb79ce70bcf858699283dae
liuxiangwin <liuxiangwin@163.com>
2015-07-02 20:04:18 +0800

    Alan Trails frist check in

---
+++ alantrails/alantrailscockpits/src/de/hybris/alantrails/cockpits/constants/AlantrailsCockpitsConstants.java
@@ -0,0 +1,29 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2014 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ *
+ *  
+ */
+package de.hybris.alantrails.cockpits.constants;
+
+/**
+ * Global class for all AlantrailsCockpits constants. You can add global constants for your extension into this class.
+ */
+public final class AlantrailsCockpitsConstants extends GeneratedAlantrailsCockpitsConstants
+{
+	public static final String EXTENSIONNAME = "alantrailscockpits";
+
+	public final static String JASPER_REPORTS_MEDIA_FOLDER = "jasperreports";
+
+	private AlantrailsCockpitsConstants()
+	{
+		// empty
+	}
+}
---
+++ alantrails/alantrailsinitialdata/src/de/hybris/alantrails/initialdata/constants/AlantrailsInitialDataConstants.java
@@ -0,0 +1,27 @@
+/*
+ * [y] hybris Platform
+ *
+ * Copyright (c) 2000-2014 hybris AG
+ * All rights reserved.
+ *
+ * This software is the confidential and proprietary information of hybris
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with hybris.
+ *
+ *  
+ */
+package de.hybris.alantrails.initialdata.constants;
+
+/**
+ * Global class for all AlantrailsInitialData constants.
+ */
+public final class AlantrailsInitialDataConstants extends GeneratedAlantrailsInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "alantrailsinitialdata";
+
+	private AlantrailsInitialDataConstants()
+	{
+		//empty
+	}
+}
